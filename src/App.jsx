1a104c609a9d35c10d28a93e11440de2c05e3d3c
Saminathan-77 <csaminathan2023@gmail.com>
2024-06-17 21:20:38 +0530

    created Header component and made changes in routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import About1 from "./pages/About1";
+import Header from "./components/Header1";
 
 export default function App() {
   return (
   <BrowserRouter>
+    <Header />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/sign-in" element={<SignIn />} />
