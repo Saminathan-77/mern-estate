6773c16c5fa887efbf99b5230f82dfa8d2a3591c
Saminathan-77 <csaminathan2023@gmail.com>
2024-06-21 14:46:19 +0530

    comlpete user listings functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -15,7 +15,7 @@ export const updateUser = async (req, res, next) => {
     return next(errorHandler(401, "You can only update ypur own account"));
   try {
     if (req.body.password) {
-      req.body.passowrd = bcryptjs.hashSync(req.body.password, 10);
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
     }
 
     const updateUser = await User.findByIdAndUpdate(
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -26,6 +26,7 @@ import { Link } from "react-router-dom";
 export default function Profile() {
   const dispatch = useDispatch();
   const fileRef = useRef(null);
+  const [userListings, setUserListings] = useState([]);
   const { currentUser, loading, error } = useSelector((state) => state.user);
   const [file, setFile] = useState(undefined);
   const [filePerc, setFilePerc] = useState(0);
@@ -33,6 +34,7 @@ export default function Profile() {
   const [formData, setFormData] = useState({});
   console.log(formData);
   const [updateSucc, setUpdateSucc] = useState(false);
+  const [showListingsError, setShowListingsError] = useState(false);
   //firebase storage
   // allow read;
   // allow write: if
@@ -128,6 +130,21 @@ export default function Profile() {
     }
   };
 
+  const showListings = async () => {
+    try {
+      setShowListingsError(false);
+      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+      if (data.success === false) {
+        setShowListingsError(true);
+        return;
+      }
+      setUserListings(data);
+    } catch (error) {
+      setShowListingsError(true);
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -212,6 +229,46 @@ export default function Profile() {
         {updateSucc ? "User updated successfully" : ""}
       </p>
       <p className="text-red-600 mt-5">{error ? error : ""}</p>
+      <button
+        onClick={showListings}
+        className="text-green-600 w-full hover:underline"
+      >
+        Show Listings
+      </button>
+      <p className="text-red-600 mt-5">
+        {showListingsError ? "Error showing Listings" : ""}
+      </p>
+      {userListings && userListings.length > 0 && (
+        <div className="flex flex-col gap-4">
+          <h1 className="text-center mt-6 text-2xl font-semibold">
+            Your Listings
+          </h1>
+          {userListings.map((listing) => (
+            <div
+              key={listing._id}
+              className="border rounded-lg p-3 flex justify-between items-center gap-4"
+            >
+              <Link to={`/listing/${listing._id}`}>
+                <img
+                  src={listing.imageUrls[0]}
+                  alt="listing cover"
+                  className="h-16 w-16 object-contain"
+                />
+              </Link>
+              <Link
+                className="flex-1 text-slate-700 font-semibold hover:underline truncate"
+                to={`/listing/${listing._id}`}
+              >
+                <p>{listing.name}</p>
+              </Link>
+              <div className="flex flex-col items-center">
+                <button className="text-red-600 uppercase">Delete</button>
+                <button className="text-green-600 uppercase">Edit</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
