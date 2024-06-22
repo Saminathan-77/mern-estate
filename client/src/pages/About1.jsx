b89d8ee296dba3f67bda6f9184e181bc0d464354
Saminathan-77 <csaminathan2023@gmail.com>
2024-06-22 21:11:39 +0530

    created home and about page

--- client/src/pages/About1.jsx
+++ client/src/pages/About1.jsx
@@ -1,5 +1,42 @@
 import React from "react";
 
 export default function About1() {
-  return <div>About</div>;
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About SamEstate
+      </h1>
+      <p className="mv-4 text-slate-700">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Nulla posuere
+        sollicitudin aliquam ultrices sagittis. Amet justo donec enim diam
+        vulputate ut. Cursus euismod quis viverra nibh cras pulvinar mattis nunc
+        sed. Eros donec ac odio tempor orci dapibus ultrices in iaculis.
+        Consequat id porta nibh venenatis cras. Iaculis eu non diam phasellus
+        vestibulum lorem sed. Amet volutpat consequat mauris nunc. Aliquam sem
+        et tortor consequat id porta nibh. Arcu ac tortor dignissim convallis
+        aenean et tortor at. Ut consequat semper viverra nam libero justo
+        laoreet. Fermentum et sollicitudin ac orci phasellus egestas tellus
+        rutrum. Donec pretium vulputate sapien nec.
+      </p>
+      <br />
+      <p className="mv-4 text-slate-700">
+        Tempus quam pellentesque nec nam aliquam. Eu lobortis elementum nibh
+        tellus. Laoreet suspendisse interdum consectetur libero id faucibus nisl
+        tincidunt eget. Tellus molestie nunc non blandit massa enim nec dui
+        nunc. Ac turpis egestas maecenas pharetra convallis posuere morbi leo.
+        Morbi quis commodo odio aenean sed. Laoreet sit amet cursus sit amet
+        dictum sit amet justo. Egestas integer eget aliquet nibh praesent
+        tristique. Pellentesque habitant morbi tristique senectus et netus et.
+        Tellus orci ac auctor augue. Tristique magna sit amet purus gravida quis
+        blandit. Vulputate sapien nec sagittis aliquam malesuada bibendum arcu.
+        Egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices. Est
+        ante in nibh mauris cursus mattis molestie a. Ut tortor pretium viverra
+        suspendisse potenti. Proin nibh nisl condimentum id venenatis. Ut tortor
+        pretium viverra suspendisse potenti nullam. Euismod in pellentesque
+        massa placerat duis ultricies lacus sed turpis. Donec massa sapien
+        faucibus et molestie ac feugiat sed lectus.
+      </p>
+    </div>
+  );
 }
